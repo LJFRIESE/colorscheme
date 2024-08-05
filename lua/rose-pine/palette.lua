c3dd258297ed4cddc26ad237ecd40f57d0b07731
LJFRIESE <135739244+LJFRIESE@users.noreply.github.com>
2024-08-05 10:31:48 -0700

    Update palette.lua

--- lua/rose-pine/palette.lua
+++ lua/rose-pine/palette.lua
@@ -1,45 +1,5 @@
 local options = require("rose-pine.config").options
 local variants = {
-	main = {
-		_nc = "#16141f",
-		base = "#191724",
-		surface = "#1f1d2e",
-		overlay = "#26233a",
-		muted = "#6e6a86",
-		subtle = "#727072",
-		text = "#e0def4",
-		love = "#eb6f92",
-		gold = "#f6c177",
-		rose = "#ebbcba",
-		pine = "#31748f",
-		foam = "#9ccfd8",
-		iris = "#c4a7e7",
-		leaf = "#95b1ac",
-		highlight_low = "#21202e",
-		highlight_med = "#403d52",
-		highlight_high = "#524f67",
-		none = "NONE",
-	},
-	moon = {
-		_nc = "#1f1d30",
-		base = "#232136",
-		surface = "#2a273f",
-		overlay = "#393552",
-		muted = "#6e6a86",
-		subtle = "#908caa",
-		text = "#e0def4",
-		love = "#eb6f92",
-		gold = "#f6c177",
-		rose = "#ea9a97",
-		pine = "#3e8fb0",
-		foam = "#9ccfd8",
-		iris = "#c4a7e7",
-		leaf = "#95b1ac",
-		highlight_low = "#2a283e",
-		highlight_med = "#44415a",
-		highlight_high = "#56526e",
-		none = "NONE",
-	},
 	dawn = {
 		_nc = "#f8f0e7",
 		base = "#26292C",
@@ -66,4 +26,4 @@ if variants[options.variant] ~= nil then
 	return variants[options.variant]
 end
 
-return vim.o.background == "light" and variants.dawn or variants[options.dark_variant or "main"]
+return variants.dawn
